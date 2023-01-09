885efe6e5611bb23c4f9b2ec043a1a0fcfc2e4a4
Jency <jencykachhadiya51@gmail.com>
2023-01-09 10:22:02 +0530

    some changes on login page

--- resources/views/client_side/login.blade.php
+++ resources/views/client_side/login.blade.php
@@ -18,14 +18,14 @@
 @endsection
 
 @section('content')
-    <section class="hero-wrap hero-wrap-2" style="background-image: url({{ asset('client_side/images/bg_3.jpg') }});"
+    <section class="hero-wrap hero-wrap-2" style="background-image: url({{ asset('client_side/images/bg_3.jpg') }}); height:315px !important; "
         data-stellar-background-ratio="0.5">
         <div class="overlay"></div>
         <div class="container">
             <div class="row no-gutters slider-text align-items-end justify-content-center">
                 <div class="col-md-9 ftco-animate text-center mb-4">
                     <h1 class="mb-2 bread">Login</h1>
-                    <p class="breadcrumbs">
+                    <p class="breadcrumbs" style="margin:0 0 210px">
                         <span class="mr-2">
                             <a href="index.html">Home
                                 <i class="ion-ios-arrow-forward"></i>
