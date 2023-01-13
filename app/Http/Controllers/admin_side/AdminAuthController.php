279b84fd3c7920571882b39598ae273b8b780f63
restaurant_management <RMP@gmail.com>
2023-01-13 13:22:48 +0530

    Change admin profile

--- app/Http/Controllers/admin_side/AdminAuthController.php
+++ app/Http/Controllers/admin_side/AdminAuthController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\Admin\RegistrationRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminAuthController extends Controller
 {
@@ -50,14 +51,12 @@ class AdminAuthController extends Controller
     public function registrationStore(RegistrationRequest $request)
     {
         $data = $request->validated();
-        // dd($data['profile_img']);
+
+        // $image = $request->profile_img->store('images', ["disk" => "public"]);
         if(isset($data['profile_img'])){
             $image = $request->profile_img;
             $imageName = 'images/' . time() . '.' . $image->extension();
             $image->move(public_path('storage/images'), $imageName);
-    
-            // $image = $request->profile_img->store('images', ["disk" => "public"]);
-    
             $data['profile_img'] != null ? $data['profile_img'] = $imageName : '';
         }
 
